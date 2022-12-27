82320517c104391e3d2405747e029fdb2d3351b8
Ferhat Ersoy <36928188+Rfetha@users.noreply.github.com>
2022-12-27 13:50:53 +0300

    Add files via upload

---
+++ Linear Regression with Synthetic Data.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import tensorflow as ts
+import matplotlib.pyplot as plt
+
+def builld_module():
+    model = tf.keras.models.Sequential()
---
+++ Linear Regression with a Real Dataset.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+
+# -- Sheet --
+
+
+
+#@title Import relevant modules
+import pandas as pd
+import tensorflow as tf
+from matplotlib import pyplot as plt
+
+# The following lines adjust the granularity of reporting. 
+pd.options.display.max_rows = 10
+pd.options.display.float_format = "{:.1f}".format
+
+# Import the dataset.
+training_df = pd.read_csv(filepath_or_buffer="https://download.mlcc.google.com/mledu-datasets/california_housing_train.csv")
+
+# Scale the label.
+training_df["median_house_value"] /= 1000.0
+
+# Print the first rows of the pandas DataFrame.
+training_df.head()
+
+training_df.describe()
+
+#@title Define the functions that build and train a model
+def build_model(my_learning_rate):
+  """Create and compile a simple linear regression model."""
+  # Most simple tf.keras models are sequential.
+  model = tf.keras.models.Sequential()
+
+  # Describe the topography of the model.
+  # The topography of a simple linear regression model
+  # is a single node in a single layer.
+  model.add(tf.keras.layers.Dense(units=1, 
+                                  input_shape=(1,)))
+
+  # Compile the model topography into code that TensorFlow can efficiently
+  # execute. Configure training to minimize the model's mean squared error. 
+  model.compile(optimizer=tf.keras.optimizers.RMSprop(lr=my_learning_rate),
+                loss="mean_squared_error",
+                metrics=[tf.keras.metrics.RootMeanSquaredError()])
+
+  return model        
+
+
+def train_model(model, df, feature, label, epochs, batch_size):
+  """Train the model by feeding it data."""
+
+  # Feed the model the feature and the label.
+  # The model will train for the specified number of epochs. 
+  history = model.fit(x=df[feature],
+                      y=df[label],
+                      batch_size=batch_size,
+                      epochs=epochs)
+
+  # Gather the trained model's weight and bias.
+  trained_weight = model.get_weights()[0]
+  trained_bias = model.get_weights()[1]
+
+  # The list of epochs is stored separately from the rest of history.
+  epochs = history.epoch
+  
+  # Isolate the error for each epoch.
+  hist = pd.DataFrame(history.history)
+
+  # To track the progression of training, we're going to take a snapshot
+  # of the model's root mean squared error at each epoch. 
+  rmse = hist["root_mean_squared_error"]
+
+  return trained_weight, trained_bias, epochs, rmse
+
+print("Defined the create_model and traing_model functions.")
+
+# The following variables are the hyperparameters.
+learning_rate = 0.01
+epochs = 30
+batch_size = 30
+
+# Specify the feature and the label.
+my_feature = "total_rooms"  # the total number of rooms on a specific city block.
+my_label="median_house_value" # the median value of a house on a specific city block.
+# That is, you're going to create a model that predicts house value based 
+# solely on total_rooms.  
+
+# Discard any pre-existing version of the model.
+my_model = None
+
+# Invoke the functions.
+my_model = build_model(learning_rate)
+weight, bias, epochs, rmse = train_model(my_model, training_df, 
+                                         my_feature, my_label,
+                                         epochs, batch_size)
+
+print("\nThe learned weight for your model is %.4f" % weight)
+print("The learned bias for your model is %.4f\n" % bias )
+
+plot_the_model(weight, bias, my_feature, my_label)
+plot_the_loss_curve(epochs, rmse)
+
+#@title Double-click to view a possible solution.
+
+my_feature = "population" # Pick a feature other than "total_rooms"
+
+# Possibly, experiment with the hyperparameters.
+learning_rate = 0.05
+epochs = 18
+batch_size = 3
+
+# Don't change anything below.
+my_model = build_model(learning_rate)
+weight, bias, epochs, rmse = train_model(my_model, training_df, 
+                                         my_feature, my_label,
+                                         epochs, batch_size)
+
+plot_the_model(weight, bias, my_feature, my_label)
+plot_the_loss_curve(epochs, rmse)
+
+predict_house_values(10, my_feature, my_label)
+
